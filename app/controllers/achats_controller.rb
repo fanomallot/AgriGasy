dd0d9379a3c32f769706cb893a2872494a3e9903
Mioratahina <alymiratana@gmail.com>
2019-09-11 00:14:31 +0300

    Page d'accueil (affichage des publications dispo)

---
+++ app/controllers/achats_controller.rb
@@ -0,0 +1,25 @@
+class AchatsController < ApplicationController
+
+  def index
+    @achat = Achat.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'accueils/accueil'
+  get 'achats/index'
+  get 'achats/show'
+  get 'achats/new'
+  get 'achats/create'
+  get 'achats/edit'
+  get 'achats/update'
+  get 'achats/destroy'
+  get 'ventes/index'
+  get 'ventes/show'
+  get 'ventes/new'
+  get 'ventes/create'
+  get 'ventes/edit'
+  get 'ventes/update'
+  get 'ventes/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
