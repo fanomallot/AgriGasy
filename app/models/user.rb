89a9f7439a32f82ff031e207c7a72c45fc93d666
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2019-09-11 00:44:59 +0300

    seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 	has_many :signalls
 	has_many :sent_messages, foreign_key: 'sender_id', class_name: "Message_prive"
 	has_many :received_messages, foreign_key: 'recipient_id', class_name: "Message_prive"
-
+	has_secure_password
 	validates :first_name, length: {in: 2..30}
 	validates :last_name, length: {in: 2..30}, presence: true
 	validates :ville, presence: true
