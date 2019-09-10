d689417cfa0f8e123e958e9828363f15b6212e2d
Mioratahina <alymiratana@gmail.com>
2019-09-11 01:28:35 +0300

    brancher la root pour tester le fonctionnement de la page d'accueil

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'accueils/accueil'
-  get 'achats/index'
-  get 'achats/show'
-  get 'achats/new'
-  get 'achats/create'
-  get 'achats/edit'
-  get 'achats/update'
-  get 'achats/destroy'
-  get 'ventes/index'
-  get 'ventes/show'
-  get 'ventes/new'
-  get 'ventes/create'
-  get 'ventes/edit'
-  get 'ventes/update'
-  get 'ventes/destroy'
+  root to: 'accueils#accueil'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
