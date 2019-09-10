af9bf407b37ad7861a9a1df0d44fc6d089fcf7ab
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2019-09-11 02:26:41 +0300

    show user

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   get 'ventes/update'
   get 'ventes/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
+
