122ddaefcd25a76a4cbd12dfc435c099fd6716d5
Mioratahina <alymiratana@gmail.com>
2019-09-11 03:53:06 +0300

    Page creating publication (vente et achat)

--- app/controllers/achats_controller.rb
+++ app/controllers/achats_controller.rb
@@ -9,9 +9,26 @@ class AchatsController < ApplicationController
   end
 
   def new
+    @produit = Produit.new
+    @region = Region.new
+    @achat = Achat.new
   end
 
   def create
+    @produit = Produit.create(name: params[:nom])
+    # if Region.find_by(place: params[:place]) == nil
+      @region = Region.create(place: params[:place])
+    # else
+    #   @region = Region.find_by(place: params[:place])
+    # end
+    @achat = achat.new(nom: params[:nom], description: params[:description],
+      quantite: params[:quantite], prix: params[:prix], date: params[:date],
+      lieu: params[:lieu], user: current_user, region_id: @region.id, produit_id: @produit.id)
+    if @achat.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   def edit
--- app/controllers/ventes_controller.rb
+++ app/controllers/ventes_controller.rb
@@ -9,9 +9,26 @@ class VentesController < ApplicationController
   end
 
   def new
+    @produit = Produit.new
+    @region = Region.new
+    @vente = Vente.new
   end
 
   def create
+    @produit = Produit.create(name: params[:nom])
+    # if Region.find_by(place: params[:place]) == nil
+      @region = Region.create(place: params[:place])
+    # else
+    #   @region = Region.find_by(place: params[:place])
+    # end
+    @vente = Vente.new(nom: params[:nom], description: params[:description],
+      quantite: params[:quantite], prix: params[:prix], date: params[:date],
+      lieu: params[:lieu], user: current_user, region_id: @region.id, produit_id: @produit.id)
+    if @vente.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   def edit
