8205c739f37b39a25ea65b9461510c8f1d38410b
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2019-09-12 09:39:06 +0300

    show region with boutton delete

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -9,4 +9,11 @@ class RegionsController < ApplicationController
 		@achats = Achat.where(region_id: @region.id)
 		@ventes = Vente.where(region_id: @region.id)
 	end
+
+	def destroy
+		@region = Region.find(params[:id])
+		@region.destroy
+		flash[:success] = "Region supprimer!"
+		redirect_to regions_path
+	end
 end
