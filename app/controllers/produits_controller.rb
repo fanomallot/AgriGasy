d2f15821dfdb88d9fd9a882dc830ad4944c9579b
Mioratahina <alymiratana@gmail.com>
2019-09-12 09:54:24 +0300

    destroy produit

--- app/controllers/produits_controller.rb
+++ app/controllers/produits_controller.rb
@@ -1,8 +1,21 @@
 class ProduitsController < ApplicationController
+
   def index
   	@produit = Produit.all
   end
+
   def show
   	@produit = Produit.find(params[:id])
   end
+
+  def destroy
+  	@produit = Produit.find(params[:id])
+  	@vente = Vente.where(produit_id: @produit)
+  	@vente.destroy_all
+  	@achat = Achat.where(produit_id: @produit)
+  	@achat.destroy_all
+  	@produit.destroy
+  	redirect_to produits_path
+  end
+
 end
