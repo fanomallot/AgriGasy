9523daaa2e24acb10189473a65d435766e74f54e
fano <aafano19@gmail.com>
2019-09-14 12:36:48 +0300

    Mvp forcé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_145204) do
+ActiveRecord::Schema.define(version: 2019_09_13_132647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2019_09_13_145204) do
     t.bigint "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "check"
+    t.string "title"
     t.boolean "is_read"
     t.index ["recipient_id"], name: "index_message_prives_on_recipient_id"
     t.index ["sender_id"], name: "index_message_prives_on_sender_id"
