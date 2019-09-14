3bcfd0bf0789221b8d23b44d9e1bb12ae8d65c77
fanomezana4523 <amicet.ando@gmail.com>
2019-09-14 09:45:16 +0300

    v modif

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_09_13_145618) do
-
+ActiveRecord::Schema.define(version: 2019_09_13_145204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,9 +58,8 @@ ActiveRecord::Schema.define(version: 2019_09_13_145618) do
     t.bigint "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
+    t.boolean "check"
     t.boolean "is_read"
-
     t.index ["recipient_id"], name: "index_message_prives_on_recipient_id"
     t.index ["sender_id"], name: "index_message_prives_on_sender_id"
   end
