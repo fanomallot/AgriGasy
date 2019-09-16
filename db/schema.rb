38210cdedacd58c8d5483bb40de6c1df337c6b16
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2019-09-16 20:09:53 +0300

    "nsoloina"

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2019_09_16_122214) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "achats", force: :cascade do |t|
-    t.string "nom"
     t.text "description"
     t.string "quantite"
     t.string "prix"
@@ -53,18 +51,6 @@ ActiveRecord::Schema.define(version: 2019_09_16_122214) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "add_isauthenticate_to_achats", force: :cascade do |t|
-    t.boolean "is_authenticate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "add_isauthenticate_to_ventes", force: :cascade do |t|
-    t.boolean "is_authenticate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "message_prives", force: :cascade do |t|
@@ -130,7 +116,6 @@ ActiveRecord::Schema.define(version: 2019_09_16_122214) do
   end
 
   create_table "ventes", force: :cascade do |t|
-    t.string "nom"
     t.text "description"
     t.string "quantite"
     t.string "prix"
