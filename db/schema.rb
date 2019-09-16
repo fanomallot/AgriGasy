70d9df8f341c3c936c83e7957ca68fc624d991e2
fano <aafano19@gmail.com>
2019-09-16 15:30:05 +0300

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_16_081758) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "achats", force: :cascade do |t|
-    t.string "nom"
     t.text "description"
     t.string "quantite"
     t.string "prix"
@@ -74,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_081758) do
     t.bigint "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.boolean "is_read"
     t.index ["recipient_id"], name: "index_message_prives_on_recipient_id"
     t.index ["sender_id"], name: "index_message_prives_on_sender_id"
@@ -124,7 +121,6 @@ ActiveRecord::Schema.define(version: 2019_09_16_081758) do
   end
 
   create_table "ventes", force: :cascade do |t|
-    t.string "nom"
     t.text "description"
     t.string "quantite"
     t.string "prix"
