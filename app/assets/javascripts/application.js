ed69eb2ad109ad6db7d680f68eac12c1b3696afe
fano <aafano19@gmail.com>
2019-09-18 11:27:20 +0300

    layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,21 +15,6 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-function myFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("mySearch");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
 $(document).ready(function() {
  $('.search').hide();
   var check = true;
